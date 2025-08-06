f86c2fbcf050935ea441c3aa20a69311188c689a
Dainis <gatesby@protonmail.com>
2025-08-06 21:54:49 +0300

    added extras

--- cogs/modassistance.py
+++ cogs/modassistance.py
@@ -64,31 +64,34 @@ class Assistance(commands.Cog):
     @app_commands.guilds(discord.Object(id=server_id))
     async def assistance(self, interaction:discord.Interaction, urgency:Literal["Low","Medium", "High"], roblox_username: str, description:str, server_era: str):
         embed = discord.Embed(title=f"Assistance needed in {server_era}!",
+                      url=f"https://www.roblox.com/users/{getUserId(roblox_username)}/profile",
                       colour=0x00b0f4)
 
         embed.set_author(name=interaction.user,
+                         url=f"https://www.roblox.com/users/{getUserId(roblox_username)}/profile",
                          icon_url=interaction.user.avatar.url)
         
         embed.add_field(name="Urgency",
                         value=urgency,
                         inline=False)
+
         embed.add_field(name="Caller",
                         value=f"{interaction.user} ({roblox_username})",
                         inline=False)
         embed.add_field(name="description",
                 value=description,
-                inline=false)
+                inline=False)
 
         embed.add_field(name="Era",
                 value=server_era,
-                inline=false)
+                inline=False)
         embed.set_thumbnail(url=getHeadshot(roblox_username))
 
         assistance_channel_parsed = interaction.client.get_channel(int(assistance_channel))
 
         message = await assistance_channel_parsed.send(embed=embed)
         thread = await message.create_thread(name=f"{urgency} :: {roblox_username}")
-        await thread.send(f"<@{interaction.user.id}>")
+        await thread.send(f"<@{interaction.user.id}> :: <@&1030362803757924452>")
         await interaction.response.send_message("Sent!", ephemeral=True)
 
 async def setup(bot: commands.Bot):
